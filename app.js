4b66b69895164913057835adecf041275be6d23a
adamant-al <adamantim@yandex.ru>
2021-06-10 12:51:16 +0300

    Clean up

--- app.js
+++ app.js
@@ -7,44 +7,8 @@ const blocksChecker = require('./modules/blocksChecker');
 require('./server');
 
 setTimeout(async () => {
-
-	// const api = require('./helpers/api');
-
-	// api.get('transactions/get', { id: '12154642911137703318', returnAsset: 1 }).then(response => {
-	// 	if (response.success) {
-	// 		const chat = response.data.transaction.asset.chat;
-	// 		if (chat) {
-	// 			console.log(1)
-	// 			msg = api.decodeMsg(chat.message, response.data.transaction.senderPublicKey, config.passPhrase, chat.own_message);
-	// 			console.log(msg)
-	// 		}
-	// 	}
-	// })
-
-	// let address = 'U14984117450827398783'
-	// let amount = 0.0000000114
-
-	// let payment = await api.sendTokens(config.passPhrase, address, amount);
-	// if (payment.success) {
-	// 		log.log(`Successfully payed ${amount.toFixed(8)} ADM reward to ${address} with Tx ${payment.data.transactionId}.`);
-	// } else {
-	// 	log.warn(`Failed to process payment of ${amount} ADM reward to ${address}. ${payment.errorMessage}.`);
-	// }
-
-	// let pub = await api.getPublicKey(address);
-	// console.log('pub', pub)
-
-
-	// payment = await api.sendMessage(config.passPhrase, address, '{"type":"eth_transaction","amount":"0.0021","hash":"10xfa46db3c99878f1f9863fcbdb0bc27d220d7065c6528543cbb83ced84487deb","comments":"I like to send it, send it"}', 'basic', amount);
-	// if (payment.success) {
-	// 		log.log(`Successfully sent message to ${address} with Tx ${payment.data.transactionId}.`);
-	// } else {
-	// 	log.warn(`Failed to sent message to ${address}. ${payment.errorMessage}`);
-	// }
-
 	await initDelegate();
-	blocksChecker();
-	
+	blocksChecker();	
 }, 0);
 
 async function initDelegate() {
